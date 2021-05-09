0d59a07b6186df9aca1009c00e5bc1736eb52057
vanessa.cardoso <vanessacardoso.santos@gmail.com>
2021-05-09 18:18:15 -0300

    Balanceamento de carga com eureka server

---
+++ hr-payroll/src/main/java/com/hrpayroll/hrpayroll/controller/PaymentResource.java
@@ -0,0 +1,24 @@
+package com.hrpayroll.hrpayroll.controller;
+
+import com.hrpayroll.hrpayroll.entity.Payment;
+import com.hrpayroll.hrpayroll.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentResource {
+
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping("/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+}
